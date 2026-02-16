1d8696996964d6dd26b458b3591c09bd76cc8742
Gihaluf <gihaluf.bojar@gmail.com>
2026-02-16 18:19:06 +0200

    Iesākta formatīvā darba pildīšana, ir izeidotas sirsniņas, kas pazūd

--- Assets/Scripts/BeanScripts/ObjectCatch.cs
+++ Assets/Scripts/BeanScripts/ObjectCatch.cs
@@ -4,6 +4,9 @@ public class ObjectCatchScript : MonoBehaviour
 {
     public float sizeIncrease = 0.5f;
     public float massIncrease = 1f;
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
     private Rigidbody2D rb;
     SFX_Script sfx;
 
@@ -14,7 +17,7 @@ public class ObjectCatchScript : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-
+    private int heart = 3;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.IsChildOf(transform))
@@ -22,13 +25,33 @@ public class ObjectCatchScript : MonoBehaviour
 
         if (collision.CompareTag("Donut"))
         {
-            sfx.PlaySFX(2);
+            sfx.PlaySFX(3);
             Destroy(collision.gameObject);
             transform.localScale += new Vector3(sizeIncrease, sizeIncrease, 0);
             rb.mass += massIncrease;
 
         }
-        else
-            Debug.Log("Collided with non-donut object: " + collision.gameObject.name);
+        else if (collision.CompareTag("Bad"))
+        {
+            if (heart == 0)
+            {
+
+            }
+            sfx.PlaySFX(4);
+            Destroy(collision.gameObject);
+            heart--;
+            switch(heart)
+            {
+                case 2:
+                    heart3.SetActive(false);
+                    break;
+                case 1:
+                    heart2.SetActive(false);
+                    break;
+                case 0:
+                    heart1.SetActive(false);
+                    break;
+            }
+        }
     }
 }
